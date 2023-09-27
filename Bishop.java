493bc011b0c144c80f09f9b46aa5fda6e77a9780
rkatz19 <rkatz219@gmail.com>
2023-09-27 18:18:01 -0400

    Added Bishop and Rook

---
+++ Bishop.java
@@ -0,0 +1,11 @@
+package chess;
+
+public class Bishop extends ReturnPiece {
+    public Bishop (PieceType type, PieceFile file, int rank) {
+        super(type, file, rank);
+    }
+
+    public void checkSpaces (String destination) {
+
+    }
+}
---
+++ Rook.java
@@ -0,0 +1,11 @@
+package chess;
+
+public class Rook extends ReturnPiece {
+	public Rook (PieceType type, PieceFile file, int rank) {
+		super(type, file, rank);
+	}
+
+	public void checkSpaces (String destination) {
+
+	}
+}
