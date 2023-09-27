2f1811e78e3dcb89f10ee7610e5b3f0e4a54fd87
InfernoAraxer <AT212355@gmail.com>
2023-09-27 17:27:24 -0400

    Sorted Classes

--- Chess.java
+++ Chess.java
@@ -1,38 +1,4 @@
-package chess;
-
-import java.util.ArrayList;
-
-class ReturnPiece {
-	static enum PieceType {WP, WR, WN, WB, WQ, WK, 
-		            BP, BR, BN, BB, BK, BQ};
-	static enum PieceFile {a, b, c, d, e, f, g, h};
-	
-	PieceType pieceType;
-	PieceFile pieceFile;
-	int pieceRank;  // 1..8
-	public String toString() {
-		return ""+pieceFile+pieceRank+":"+pieceType;
-	}
-	public boolean equals(Object other) {
-		if (other == null || !(other instanceof ReturnPiece)) {
-			return false;
-		}
-		ReturnPiece otherPiece = (ReturnPiece)other;
-		return pieceType == otherPiece.pieceType &&
-				pieceFile == otherPiece.pieceFile &&
-				pieceRank == otherPiece.pieceRank;
-	}
-}
-
-class ReturnPlay {
-	enum Message {ILLEGAL_MOVE, DRAW, 
-				  RESIGN_BLACK_WINS, RESIGN_WHITE_WINS, 
-				  CHECK, CHECKMATE_BLACK_WINS,	CHECKMATE_WHITE_WINS, 
-				  STALEMATE};
-	
-	ArrayList<ReturnPiece> piecesOnBoard;
-	Message message;
-}
+package Chess;
 
 public class Chess {
 	
